12a3873e7be1f83ecef162c7e9e5f409a7eda506
Meet Jain <133582566+Meetjain1@users.noreply.github.com>
2025-04-19 10:26:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,15 +5,11 @@ dotenv.config();
 import 'express-async-errors';
 import morgan from 'morgan';
 
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import path from 'path';
-
 import helmet from 'helmet';
 import xss from 'xss-clean';
 import mongoSanitize from 'express-mongo-sanitize';
 import cookieParser from 'cookie-parser';
-// hello
+
 // db and authenticateUser
 import connectDB from './db/connect.js';
 
@@ -30,11 +26,6 @@ if (process.env.NODE_ENV !== 'production') {
   app.use(morgan('dev'));
 }
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-// only when ready to deploy
-app.use(express.static(path.resolve(__dirname, './client/build')));
-
 app.use(express.json());
 app.use(helmet());
 app.use(xss());
@@ -43,11 +34,6 @@ app.use(cookieParser());
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
-
-// only when ready to deploy
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
-});
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
